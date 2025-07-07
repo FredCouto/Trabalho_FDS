f441cbab7c699a6f95584aa9c600fe2128e59c17
FredCouto <fredcoutomendes11@gmail.com>
2025-07-07 15:04:17 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const botao = document.querySelector('#botao');
-
-botao.addEventListener('click', e => {
-    document.querySelector('#saudacao').textContent = "Eu espero que você tenha um ótimo dia!";
-});
